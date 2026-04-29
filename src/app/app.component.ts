05795bec873ab3ce8341b15abdff0ed5d6de43a2
Morgana <mmr209@alu.ua.es>
2026-04-29 10:55:49 +0200

    Hemos avanzado algo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Lista } from './models/lista';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AppListas';
+  msg = "Hola, bienvenido a mi proyecto en Angular";
+  listas: Lista[] = [];
+  nueva: Lista = new Lista();
+  mostrarFormulario = false;
+
+  ngOnInit(): void {
+    setTimeout(() =>  {this.msg = ''}, 5000);
+  }
+
+  nuevaLista() {
+    this.mostrarFormulario = true;
+  }
 }
---
+++ src/app/models/lista.ts
@@ -0,0 +1,13 @@
+export class Lista {
+    nombre: string;
+    descripcion: string;
+    color: string;
+    visible: boolean;
+
+    constructor() {
+        this.nombre="";
+        this.descripcion="";
+        this.color="#FFFFFF";
+        this.visible=true;
+    }
+}
