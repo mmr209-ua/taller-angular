2bfda9cb588aff115f1d4cb751714dc16f0849d8
Sara <svp39@alu.ua.es>
2026-04-29 18:21:45 +0200

    Implementado borrar lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Lista } from './models/lista';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'AppListas';
@@ -12,6 +14,9 @@ export class AppComponent {
   listas: Lista[] = [];
   nueva: Lista = new Lista();
   mostrarForm = false;
+  listaSeleccionada?: Lista;
+
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {
     setTimeout(() =>  {this.msg = ''}, 5000);
@@ -37,5 +42,18 @@ export class AppComponent {
 
   ocultarDetalle(lista: Lista): void {
     lista.visible = false;
+  }
+
+  abrirModalEliminar(content: TemplateRef<unknown>, lista: Lista): void {
+    this.listaSeleccionada = lista;
+    this.modalService.open(content, { centered: true });
+  }
+
+  confirmarEliminacion(modal: { close: () => void }): void {
+    if (this.listaSeleccionada) {
+      this.listas = this.listas.filter((lista) => lista !== this.listaSeleccionada);
+      this.listaSeleccionada = undefined;
+    }
+    modal.close();
   }
 }
